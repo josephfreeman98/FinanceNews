811f4f3f54e740ebcac2fbdec3bc2c4ca5b9872b
Szabados Dodi <szabados.jozsefd@gmail.com>
2022-09-26 15:01:38 +0200

    First commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using FinanceNewsTicker.Models;
+using FinanceNewsTicker.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FinanceNewsTicker.Pages {
+    public class IndexModel : PageModel {
+
+        public FinanceNews news;
+
+        private readonly ILogger<IndexModel> _logger;
+        private readonly INewsService _newsService;
+
+        public IndexModel(ILogger<IndexModel> logger, INewsService newsService) {
+            _logger = logger;
+            _newsService = newsService;
+        }
+
+        public void OnGet() {
+
+            news = _newsService.GetFinanceNews(0);
+        }
+
+
+        public void OnGetLoadMoreNews(int offset) {
+            news = _newsService.GetFinanceNews(offset);
+
+
+        }
+    }
+}
